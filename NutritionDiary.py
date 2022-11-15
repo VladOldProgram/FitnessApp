07c34e3f358cae8d97a6fa9d2bed21bc3f876c37
AnnaZhidkova9 <86468434+AnnaZhidkova9@users.noreply.github.com>
2022-11-15 23:02:35 +0300

    Update NutritionDiary.py

--- NutritionDiary.py
+++ NutritionDiary.py
@@ -10,11 +10,42 @@ class Example(tk.Frame):
         self.init_ui()
 
     def init_ui(self):
-        btn_add2 = tk.Button(self, text='Удалить зиз дневника питания')
+        btn_add2 = tk.Button(self, text='Удалить из дневника питания')
         btn_add2.place(x=900, y=770)
 
         label_txtMealWeight = tk.Label(self, text='Дневник питания', font='Arial 15 bold')
         label_txtMealWeight.place(x=710, y=0)
+
+        # поле для добавления продукта/блюда
+        frame_foodstruct_add_product = tk.LabelFrame(self, width=430, height=230)
+        frame_foodstruct_add_product.place(x=30, y=10)
+        label_foodstruct_add_product = tk.Label(frame_foodstruct_add_product, text='Добавление продукта/блюда', font=("Arial", 10))
+        label_foodstruct_add_product.place(x=100, y=10)
+        product_weight_stepper_input = ttk.Entry(frame_foodstruct_add_product, font=("Arial", 10), width=10)
+        product_weight_stepper_input.place(x=160, y=55)
+
+        # поисковая строка
+        foodstruct_search_line = tk.Entry(frame_foodstruct_add_product, fg='Grey')
+        entry_text2 = 'Поиск продуктов/блюд'
+        foodstruct_search_line.insert(0, entry_text2)
+        foodstruct_search_line.bind("<FocusIn>", lambda args: focus_in_entry_box(foodstruct_search_line))
+        foodstruct_search_line.bind("<FocusOut>", lambda args: focus_out_entry_box(foodstruct_search_line, entry_text2))
+        foodstruct_search_line.place(x=20, y=40, width=300)
+
+        # создание placeholder...
+        def focus_out_entry_box(frame_foodstruct_add_product, frame_foodstruct_add_product_text):
+            if frame_foodstruct_add_product['fg'] == 'Black' and len(frame_foodstruct_add_product.get()) == 0:
+                frame_foodstruct_add_product.delete(0, tk.END)
+                frame_foodstruct_add_product['fg'] = 'Grey'
+                frame_foodstruct_add_product.insert(0, frame_foodstruct_add_product_text)
+
+        def focus_in_entry_box(frame_foodstruct_add_product):
+            if frame_foodstruct_add_product['fg'] == 'Grey':
+                frame_foodstruct_add_product['fg'] = 'Black'
+                frame_foodstruct_add_product.delete(0, tk.END)
+
+        searchbtn = tk.Button(frame_foodstruct_add_product, text="Найти", font=("Arial", 10))
+        searchbtn.place(x=350, y=40)
 
         frame = ttk.Frame(self)
         frame.place(x=500, y=40)
