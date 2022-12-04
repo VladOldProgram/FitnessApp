fec3eba2c7439dd01d427c93789c7babe1167eb0
AnnaZhidkova9 <86468434+AnnaZhidkova9@users.noreply.github.com>
2022-12-04 19:00:37 +0300

    Update NutritionDiary.py

--- NutritionDiary.py
+++ NutritionDiary.py
@@ -22,9 +22,11 @@ class Example(tk.Frame):
         label_foodstruct_add_product = tk.Label(frame_foodstruct_add_product, text='Добавление продукта/блюда', font=("Arial", 10))
         label_foodstruct_add_product.place(x=100, y=10)
         product_weight_stepper_input = ttk.Entry(frame_foodstruct_add_product, font=("Arial", 10), width=10)
-        product_weight_stepper_input.place(x=160, y=55)
+        product_weight_stepper_input.place(x=320, y=105)
+        add_product_button = tk.Button(frame_foodstruct_add_product, text="Добавить в дневник питания", font=("Arial", 10))
+        add_product_button.place(x=210, y=170)
 
-        # поисковая строка
+        # поисковая строка продуктов/блюд
         foodstruct_search_line = tk.Entry(frame_foodstruct_add_product, fg='Grey')
         entry_text2 = 'Поиск продуктов/блюд'
         foodstruct_search_line.insert(0, entry_text2)
@@ -33,20 +35,58 @@ class Example(tk.Frame):
         foodstruct_search_line.place(x=20, y=40, width=300)
 
         # создание placeholder...
-        def focus_out_entry_box(frame_foodstruct_add_product, frame_foodstruct_add_product_text):
-            if frame_foodstruct_add_product['fg'] == 'Black' and len(frame_foodstruct_add_product.get()) == 0:
-                frame_foodstruct_add_product.delete(0, tk.END)
-                frame_foodstruct_add_product['fg'] = 'Grey'
-                frame_foodstruct_add_product.insert(0, frame_foodstruct_add_product_text)
+        def focus_out_entry_box(self, self_text):
+            if self['fg'] == 'Black' and len(self.get()) == 0:
+                self.delete(0, tk.END)
+                self['fg'] = 'Grey'
+                self.insert(0, self_text)
 
-        def focus_in_entry_box(frame_foodstruct_add_product):
-            if frame_foodstruct_add_product['fg'] == 'Grey':
-                frame_foodstruct_add_product['fg'] = 'Black'
-                frame_foodstruct_add_product.delete(0, tk.END)
+        def focus_in_entry_box(self):
+            if self['fg'] == 'Grey':
+                self['fg'] = 'Black'
+                self.delete(0, tk.END)
 
         searchbtn = tk.Button(frame_foodstruct_add_product, text="Найти", font=("Arial", 10))
-        searchbtn.place(x=350, y=40)
+        searchbtn.place(x=360, y=35)
 
+        # поле для добавления сохраненного блюда
+        frame_foodstruct_add_dish = tk.LabelFrame(self, width=430, height=510)
+        frame_foodstruct_add_dish.place(x=30, y=260)
+        label_foodstruct_add_dish = tk.Label(frame_foodstruct_add_dish, text='Добавление продукта/блюда', font=("Arial", 10))
+        label_foodstruct_add_dish.place(x=100, y=10)
+
+        # поисковая строка сохраненных блюд
+        saved_dishes_search_line  = tk.Entry(frame_foodstruct_add_dish, fg='Grey')
+        entry_text3 = 'Поиск сохраненных блюд'
+        saved_dishes_search_line .insert(0, entry_text2)
+        saved_dishes_search_line .bind("<FocusIn>", lambda args: focus_in_entry_box(saved_dishes_search_line ))
+        saved_dishes_search_line .bind("<FocusOut>", lambda args: focus_out_entry_box(saved_dishes_search_line , entry_text3))
+        saved_dishes_search_line .place(x=20, y=40, width=300)
+
+        searchbtn2 = tk.Button(frame_foodstruct_add_dish, text="Найти", font=("Arial", 10))
+        searchbtn2.place(x=360, y=35)
+
+        # таблица сохраненных блюд
+        frame = ttk.Frame(frame_foodstruct_add_dish)
+        frame.place(x=20, y=100)
+
+        self.tree = ttk.Treeview(frame, columns=('txtProductName', 'txtProductWeight', 'txtProductProteins', 'txtProductFats', 'txtProductCarbohydrates', 'txtProductCalories'), height=34, show='headings')
+
+        self.tree.column('txtProductName', width=150, anchor=tk.CENTER)
+        self.tree.column('txtProductWeight', width=80, anchor=tk.CENTER)
+        self.tree.column('txtProductProteins', width=80, anchor=tk.CENTER)
+        self.tree.column('txtProductFats', width=80, anchor=tk.CENTER)
+        self.tree.column('txtProductCarbohydrates', width=80, anchor=tk.CENTER)
+        self.tree.column('txtProductCalories', width=95, anchor=tk.CENTER)
+
+        self.tree.heading('txtProductName', text='Продукт')
+        self.tree.heading('txtProductWeight', text='Вес, гр.')
+        self.tree.heading('txtProductProteins', text='Белки, гр.')
+        self.tree.heading('txtProductFats', text='Жиры, гр.')
+        self.tree.heading('txtProductCarbohydrates', text='Углеводы, гр.')
+        self.tree.heading('txtProductCalories', text='Калории, ккал.')
+
+        # таблица дневника питания
         frame = ttk.Frame(self)
         frame.place(x=500, y=40)
 
@@ -67,4 +107,5 @@ class Example(tk.Frame):
         self.tree.heading('txtProductCalories', text='Калории, ккал.')
 
         self.tree.pack()
+
 
