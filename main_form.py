1585c08dc2cb17cc1ed4d8b2bf14049997bf986a
AleksandraChernova <86555416+AleksandraChernova@users.noreply.github.com>
2022-12-10 23:17:29 +0300

    Update main_form.py

--- main_form.py
+++ main_form.py
@@ -6,7 +6,7 @@ import tkinter as tk
 # import threading
 # from FAKEFUNC import *
 
-from dish_nutrients_form import Example as ReadyMeal
+from dish_nutrients_form import Dish_nutrients_form as ReadyMeal
 from daily_calories_standart_form import Daily_calories_standart as CalorieDailyRate
 from food_diary_form import Example as NutritionDiary
 
