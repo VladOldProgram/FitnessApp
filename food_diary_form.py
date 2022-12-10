327011d18d20940490c512f170a96a86d0d8a64d
AnnaZhidkova9 <86468434+AnnaZhidkova9@users.noreply.github.com>
2022-12-11 00:56:49 +0300

    Update food_diary_form.py

--- food_diary_form.py
+++ food_diary_form.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 from tkinter import messagebox as mb
 from PIL import ImageTk, Image
 from tkinter import *
+from foodstruct import *
 
 class Example(tk.Frame):
     def __init__(self, parent):
@@ -13,6 +14,40 @@ class Example(tk.Frame):
 
     def init_ui(self):
 
+        def search():
+
+            def rekomend(event):
+                print("Нажала")
+                cur_row = table2.focus()
+                vals = table2.item(cur_row, "values")
+                print("Продукт = ", str(vals[0]))
+                product_data = get_product_nutrients_data(str(vals[0]))
+                print(product_data)
+                # print(get.get(vals[0])) # ссылка на выбранный продукт
+                table2.place(x=2000, y=2000)
+
+                found_product_name_label.config(text=vals[0])
+                found_product_calories_label.config(
+                    text=product_data['calories'])
+                found_product_proteins_label.config(
+                    text=product_data['proteins'])
+                found_product_fats_label.config(text=product_data['fats'])
+                found_product_carbohydrates_label.config(
+                    text=product_data['carbohydrates'])
+
+            get = get_service_recommendations(
+                'stroka')  # получаем словарь продуктов
+            table2 = ttk.Treeview(self, columns=('1'), show="")
+            table2.place(x=28, y=50)
+            table2.column("1", width=400)
+            table2.delete(*table2.get_children())
+            for x in get.keys():  # проходимся по всем рекомендациям
+                table2.insert('', 'end', values=[x])
+            # по двойному щелчку вызываем функцию rekomend
+            table2.bind("<Double-Button-1>", rekomend)
+
+
+
         # поле для добавления продукта/блюда
         frame_foodstruct_add_product = tk.LabelFrame(self, width=430, height=230)
         frame_foodstruct_add_product.place(x=30, y=10)
@@ -21,10 +56,6 @@ class Example(tk.Frame):
         self.loupa.place(x=15, y=37)
         label_foodstruct_add_product = tk.Label(frame_foodstruct_add_product, text='Добавление продукта/блюда', font=("Arial", 10))
         label_foodstruct_add_product.place(x=100, y=10)
-        product_weight_stepper_input = ttk.Entry(frame_foodstruct_add_product, font=("Arial", 10), width=10)
-        product_weight_stepper_input.place(x=320, y=105)
-        add_product_button = tk.Button(frame_foodstruct_add_product, text="Добавить в дневник питания", font=("Arial", 10))
-        add_product_button.place(x=210, y=170)
 
         # поисковая строка продуктов/блюд
         foodstruct_search_line = tk.Entry(frame_foodstruct_add_product, fg='Grey')
@@ -46,8 +77,51 @@ class Example(tk.Frame):
                 self['fg'] = 'Black'
                 self.delete(0, tk.END)
 
-        searchbtn = tk.Button(frame_foodstruct_add_product, text="Найти", font=("Arial", 10))
+        searchbtn = tk.Button(frame_foodstruct_add_product, text="Найти", font=("Arial", 10), command=search)
         searchbtn.place(x=360, y=35)
+
+        product_weight_stepper_input = ttk.Entry(frame_foodstruct_add_product, font=("Arial", 10), width=10)
+        product_weight_stepper_input.place(x=320, y=105)
+
+        found_product_name_label = Label(frame_foodstruct_add_product, text='')
+        found_product_name_label.place(x=20, y=103)
+
+        found_product_calories_label = Label(frame_foodstruct_add_product, text='')
+        found_product_calories_label.place(x=1500, y=1000)
+
+        found_product_proteins_label = Label(frame_foodstruct_add_product, text='')
+        found_product_proteins_label.place(x=1500, y=1050)
+
+        found_product_fats_label = Label(frame_foodstruct_add_product, text='')
+        found_product_fats_label.place(x=1500, y=2000)
+
+        found_product_carbohydrates_label = Label(frame_foodstruct_add_product, text='')
+        found_product_carbohydrates_label.place(x=1500, y=2500)
+
+
+        def add_product():
+            #found_product_name_label.cget("text"), found_product_calories_label.cget("text"), found_product_proteins_label.cget("text"), found_product_fats_label.cget("text"), found_product_carbohydrates_label.cget("text"), product_weight_stepper_input.get()
+            m1=found_product_name_label.cget("text")
+            m2=found_product_calories_label.cget("text")
+            m3=found_product_proteins_label.cget("text")
+            m4=found_product_fats_label.cget("text") 
+            m5=found_product_carbohydrates_label.cget("text")
+            m6=product_weight_stepper_input.get()
+
+            print(m1)
+            print(m2)
+            print(m3)
+            print(m4)
+            print(m5)
+            print(m6)
+
+            l = [m1,m2,m3,m4,m5,m6]
+            print("лист ", l)
+
+            self.tree.insert('', 'end', values=(m1, m2,m3, m4, m5, m6))
+
+        add_product_button = tk.Button(frame_foodstruct_add_product, text="Добавить в дневник питания", font=("Arial", 10), command=add_product)
+        add_product_button.place(x=210, y=170)
 
         # поле для добавления сохраненного блюда
         frame_foodstruct_add_dish = tk.LabelFrame(self, width=430, height=540)
@@ -76,10 +150,10 @@ class Example(tk.Frame):
         self.tree2 = ttk.Treeview(frame_dish, columns=('txtDishName', 'txtDishProteins', 'txtDishFats', 'txtDishCarbohydrates', 'txtDishCalories'), height=14, show='headings')
 
         self.tree2.column('txtDishName', width=70, anchor=tk.CENTER)
-        self.tree2.column('txtDishProteins', width=70, anchor=tk.CENTER)
+        self.tree2.column('txtDishProteins', width=90, anchor=tk.CENTER)
         self.tree2.column('txtDishFats', width=70, anchor=tk.CENTER)
         self.tree2.column('txtDishCarbohydrates', width=80, anchor=tk.CENTER)
-        self.tree2.column('txtDishCalories', width=90, anchor=tk.CENTER)
+        self.tree2.column('txtDishCalories', width=70, anchor=tk.CENTER)
 
         self.tree2.heading('txtDishName', text='Блюдо')
         self.tree2.heading('txtDishProteins', text='Белки, гр.')
@@ -112,21 +186,43 @@ class Example(tk.Frame):
         frame_diary = ttk.Frame(self)
         frame_diary.place(x=500, y=40)
 
-        self.tree = ttk.Treeview(frame_diary, columns=('txtProductName', 'txtProductWeight', 'txtProductProteins', 'txtProductFats', 'txtProductCarbohydrates', 'txtProductCalories'), height=34, show='headings')
+        def delete_all():
+            if not self.tree.selection():
+                mb.showinfo('Удаление', 'Дневник питания пуст')
+            else:
+                answer = mb.askyesno(
+                    message='Вы уверены, что хотите удалить все продукты/блюда из дневника?')
+                    
+                if answer:
+                    self.tree.delete(*self.tree.get_children())
+                    mb.showinfo(message='Список продуктов очищен')
+
+        def delete_by():
+            if not self.tree.selection():
+                mb.showinfo('Удаление', 'Данные для удаления не найдены')
+            else:
+                answer = mb.askyesno(message='Вы уверены, что хотите удалить данные  о продукте/блюде?')
+                if answer:
+                    item = self.tree.selection()[0]
+                    self.tree.delete(item)
+                    self.tree.config(height=34)
+                    mb.showinfo(message='Данные удалены')
+
+        self.tree = ttk.Treeview(frame_diary, columns=('txtProductName', 'txtProductCalories', 'txtProductProteins', 'txtProductFats', 'txtProductCarbohydrates', 'txtProductWeight'), height=34, show='headings')
 
         self.tree.column('txtProductName', width=150, anchor=tk.CENTER)
-        self.tree.column('txtProductWeight', width=80, anchor=tk.CENTER)
+        self.tree.column('txtProductCalories', width=80, anchor=tk.CENTER)
         self.tree.column('txtProductProteins', width=80, anchor=tk.CENTER)
         self.tree.column('txtProductFats', width=80, anchor=tk.CENTER)
         self.tree.column('txtProductCarbohydrates', width=80, anchor=tk.CENTER)
-        self.tree.column('txtProductCalories', width=95, anchor=tk.CENTER)
+        self.tree.column('txtProductWeight', width=95, anchor=tk.CENTER)
 
         self.tree.heading('txtProductName', text='Продукт/блюдо')
-        self.tree.heading('txtProductWeight', text='Вес, гр.')
+        self.tree.heading('txtProductCalories', text='Калории, ккал.')
         self.tree.heading('txtProductProteins', text='Белки, гр.')
         self.tree.heading('txtProductFats', text='Жиры, гр.')
         self.tree.heading('txtProductCarbohydrates', text='Углеводы, гр.')
-        self.tree.heading('txtProductCalories', text='Калории, ккал.')
+        self.tree.heading('txtProductWeight', text='Вес, гр.')
 
         scroll = tk.Scrollbar(frame_diary, command=self.tree.yview)  # Линейка прокрутки для списка
         scroll.place(x=1560, y=50, height=695)
@@ -136,7 +232,10 @@ class Example(tk.Frame):
         self.tree.pack()
 
         result_calories_label = tk.Label(self, text='Суммарное количество калорий', font=("Arial", 10))
-        result_calories_label.place(x=540, y=750)
+        result_calories_label.place(x=500, y=750)
 
-        btn_add2 = tk.Button(self, text='Удалить из дневника питания')
+        btn_add2 = tk.Button(self, text='Удалить из дневника питания', command=delete_by)
         btn_add2.place(x=900, y=770)
+
+        btn_add3 = tk.Button(self, text='Очистить дневник питания', command=delete_all)
+        btn_add3.place(x=720, y=770)
