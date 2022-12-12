49f13675e22d4a691ca362698a178b44a57b94a5
AnnaZhidkova9 <86468434+AnnaZhidkova9@users.noreply.github.com>
2022-12-12 15:55:26 +0300

    Update daily_calories_standart_form.py

--- daily_calories_standart_form.py
+++ daily_calories_standart_form.py
@@ -3,7 +3,6 @@ from tkinter import ttk
 import tkinter as tk
 from calculate import * 
 
-
 class Daily_calories_standart(tk.Frame):
     def __init__(self, parent: ttk.Notebook):
         super().__init__(parent)
@@ -16,14 +15,14 @@ class Daily_calories_standart(tk.Frame):
         self.image_male = tk.PhotoImage(file='assets\\images\\male.png')
 
         self.selected_sex_image = tk.Label(self, image=self.image_male)
-        self.selected_sex_image.place(x=80, y=140)
+        self.selected_sex_image.place(x=80, y=80)
 
         self.switch_female_button = tk.Button(self, image=self.image_female_button)
-        self.switch_female_button.place(x=300, y=10)
+        self.switch_female_button.place(x=450, y=30)
         self.switch_female_button.bind('<Button-1>', self.switch_female)
 
         self.switch_male_button = tk.Button(self, image=self.image_male_button)
-        self.switch_male_button.place(x=700, y=10)
+        self.switch_male_button.place(x=750, y=30)
         self.switch_male_button.bind('<Button-1>', self.switch_male)
 
         self.activity_level_descriptions = [
@@ -33,19 +32,19 @@ class Daily_calories_standart(tk.Frame):
             'Уровень физической активности, который значительно повышает частоту сердечных сокращений и вызывает появление пота, например, усилия, затрачиваемые здоровым человеком при беге, занятиях аэробикой, плавании на дистанцию, быстрой езде на велосипеде, подъеме в гору.  Выполнение интенсивных упражнений не менее 60 минут от 5 до 7 дней в неделю или занятия 2 раза в день.',
             '7 или более часов в неделю очень интенсивных упражнений или интенсивные занятия 2 раза в день. Очень интенсивные и/или очень затратные виды деятельности. Например, профессиональный спортсмен с несколькими тренировками в течение дня или тяжелая физическая работа, такая, как разгрузка угля лопатой или работа на сборочной линии в течение нескольких часов.'
         ]
-        self.activity_level_description_text = tk.Text(self, wrap=tk.WORD, font=('Arial', 14))
+        self.activity_level_description_text = tk.Text(self, wrap=tk.WORD, font=("Arial", 10))
         self.activity_level_description_text.place(x=630, y=480, height=300, width=400)
         self.activity_level_description_text.insert(1.0, self.activity_level_descriptions[0])
         self.activity_level_description_text.configure(state=tk.DISABLED)
 
         self.selected_activity_level = tk.IntVar()
         self.selected_activity_level.set(0)
-        self.activity_level_label = tk.Label(self, text='Уровень активности:', font=('Arial', 16))
-        self.activity_level_label.place(x=743, y=200)
+        self.activity_level_label = tk.Label(self, text='Уровень активности:', font=("Arial", 10))
+        self.activity_level_label.place(x=743, y=215)
         self.activity_level_vertical_menu = tk.Radiobutton(
             self, 
             text='минимальный', 
-            font=('Arial', 16), 
+            font=('Arial', 10), 
             indicatoron=0, 
             width=20, 
             padx=60, 
@@ -57,7 +56,7 @@ class Daily_calories_standart(tk.Frame):
         self.activity_level_vertical_menu = tk.Radiobutton(
             self, 
             text='низкий', 
-            font=('Arial', 16), 
+            font=('Arial', 10), 
             indicatoron=0, 
             width=20, 
             padx=60, 
@@ -65,11 +64,11 @@ class Daily_calories_standart(tk.Frame):
             command=self.switch_activity_level_description, 
             value=1
         )
-        self.activity_level_vertical_menu.place(x=670, y=270)
+        self.activity_level_vertical_menu.place(x=670, y=265)
         self.activity_level_vertical_menu = tk.Radiobutton(
             self, 
             text='умеренный', 
-            font=('Arial', 16), 
+            font=('Arial', 10), 
             indicatoron=0, 
             width=20, 
             padx=60, 
@@ -77,63 +76,64 @@ class Daily_calories_standart(tk.Frame):
             command=self.switch_activity_level_description, 
             value=2
         )
-        self.activity_level_vertical_menu.place(x=670, y=300)
+        self.activity_level_vertical_menu.place(x=670, y=290)
         self.activity_level_vertical_menu = tk.Radiobutton(
             self, 
             text='высокий', 
-            font=('Arial', 16), 
+            font=('Arial', 10), 
             indicatoron=0, 
             width=20, 
             padx=60, 
             variable=self.selected_activity_level, 
             command=self.switch_activity_level_description, 
             value=3)
-        self.activity_level_vertical_menu.place(x=670, y=330)
+        self.activity_level_vertical_menu.place(x=670, y=315)
         self.activity_level_vertical_menu = tk.Radiobutton(
             self, 
             text='экстремальный', 
-            font=('Arial', 16), 
+            font=('Arial', 10), 
             indicatoron=0, 
             width=20, 
             padx=60, 
             variable=self.selected_activity_level, 
             command=self.switch_activity_level_description, 
             value=4)
-        self.activity_level_vertical_menu.place(x=670, y=360)
+        self.activity_level_vertical_menu.place(x=670, y=340)
 
-        self.height_label = tk.Label(self, text='Рост (см.)', font=('Arial', 12))
-        self.height_label.place(x=325, y=280)
-        self.weight_label = tk.Label(self, text='Вес (кг.)', font=('Arial', 12))
-        self.weight_label.place(x=115, y=580)
-        self.age_label_1 = tk.Label(self, text='Возраст', font=('Arial', 12))
+        self.height_label = tk.Label(self, text='Рост (см.)', font=("Arial", 10))
+        self.height_label.place(x=320, y=280)
+        self.weight_label = tk.Label(self, text='Вес (кг.)', font=("Arial", 10))
+        self.weight_label.place(x=120, y=520)
+        self.age_label_1 = tk.Label(self, text='Возраст', font=("Arial", 10))
         self.age_label_1.place(x=330, y=546)
-        self.age_label_2 = tk.Label(self, text='лет', font=('Arial', 12))
+        self.age_label_2 = tk.Label(self, text='лет', font=("Arial", 10))
         self.age_label_2.place(x=510, y=546)
 
-        self.height_entry = tk.Entry(self, font=('Arial', 12), width=15)
+        self.height_entry = tk.Entry(self, font=("Arial", 10), width=15)
         self.height_entry.place(x=300, y=260)
-        self.weight_entry = tk.Entry(self, font=('Arial', 12), width=15)
-        self.weight_entry.place(x=80, y=550)
-        self.age_entry = tk.Entry(self, font=('Arial', 12), width=11)
+        self.weight_entry = tk.Entry(self, font=("Arial", 10), width=15)
+        self.weight_entry.place(x=100, y=490)
+        self.age_entry = tk.Entry(self, font=("Arial", 10), width=11)
         self.age_entry.place(x=400, y=550)
 
-        self.label_frame = tk.LabelFrame(self, width=270, height=40, text='Суточная норма калорий', font=('Arial', 14))
-        self.label_frame.place(x=100, y=640)
+        self.label_frame = tk.LabelFrame(self, width=165, height=50, text='Суточная норма калорий', font=("Arial", 10))
+        self.label_frame.place(x=120, y=640)
+        self.label_frame.pack_propagate(False)
         self.daily_calories_standart = 0.0
-        self.daily_calories_standart_label = tk.Label(self.label_frame, text=self.daily_calories_standart, font=('Arial', 14))
+        self.daily_calories_standart_label = tk.Label(self.label_frame, text=self.daily_calories_standart, font=("Arial", 10))
 
         self.calculate_daily_calories_standart_button = tk.Button(
             self,
             text='Рассчитать', 
-            font=('Arial', 14),
+            font=("Arial", 10),
             command=self.calculate_and_save_daily_calories_standart
         )
-        self.calculate_daily_calories_standart_button.place(x=370, y=650)
+        self.calculate_daily_calories_standart_button.place(x=320, y=655)
 
         self.clear_inputs_button = tk.Button(
             self, 
             text='Очистить \n поля ввода', 
-            font=('Arial', 14), 
+            font=("Arial", 10), 
             command=self.clear_inputs
         )
         self.clear_inputs_button.place(x=100, y=730)
@@ -173,6 +173,11 @@ class Daily_calories_standart(tk.Frame):
              activity_level_coefficient
         )
         daily_calories_standart = round(daily_calories_standart, 2)
+
+
+        # result_calories_label4.config(text = daily_calories_standart)
+
+
         
         self.daily_calories_standart = daily_calories_standart
         self.daily_calories_standart_label.configure(text=daily_calories_standart)
@@ -185,6 +190,7 @@ class Daily_calories_standart(tk.Frame):
             'age': int(self.age_entry.get()),
             'activity_level': int(self.selected_activity_level.get())
         }
+        
         with open('json\daily_calories_standart.json', 'w') as outfile:
             json.dump(data, outfile)
 
