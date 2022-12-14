7306ba5f8108cbde1575e8a3d44e55ad9ab81489
Vladislav <86352975+VladOldProgram@users.noreply.github.com>
2022-12-14 23:29:39 +0300

    Update unit_tests.py

--- unit_tests.py
+++ unit_tests.py
@@ -1,6 +1,8 @@
 import unittest
 from calculate import *
 from foodstruct import *
+from dish_nutrients_form import *
+from daily_calories_standart_form import *
 
 class Test_unit_tests(unittest.TestCase):
     def test_M0(self):
@@ -68,7 +70,7 @@ class Test_unit_tests(unittest.TestCase):
         self.assertEqual(expected_result, result)
 
     def test_M4(self):
-        expected_result = 73.0 ##############
+        expected_result = 73.0
         products = {
             'Огурец': {
                 'weight': 300.0,
@@ -85,8 +87,8 @@ class Test_unit_tests(unittest.TestCase):
                 'carbohydrates': 17.06
             }
         }
-        dish_weight = 500  ##########
-        result = calculate_dish_calories(products, dish_weight) ###########
+        dish_weight = 500
+        result = calculate_dish_calories(products, dish_weight)
         self.assertEqual(expected_result, result)
 
     def test_M5(self):
@@ -194,17 +196,17 @@ class Test_unit_tests(unittest.TestCase):
         result = get_product_nutrients_data(product_name)
         self.assertDictEqual(expected_result, result)
 
-    def test_M13(self):
-        product_name = 'Булка'
-        expected_result = HTTPError
-        result = get_product_nutrients_data(product_name)
-        self.assertEqual(expected_result, result)
+    #def test_M13(self):
+    #    product_name = 'Булка'
+    #    expected_result = HTTPError
+    #    result = get_product_nutrients_data(product_name)
+    #    self.assertEqual(expected_result, result)
         
-    def test_M14(self):
-        product_name = 'Авокадо'
-        expected_result = HTTPError
-        result = get_product_nutrients_data(product_name)
-        self.assertEqual(expected_result, result)
+    #def test_M14(self):
+    #    product_name = 'Авокадо'
+    #    expected_result = HTTPError
+    #    result = get_product_nutrients_data(product_name)
+    #    self.assertEqual(expected_result, result)
 
     def test_M15(self):
         product_name = 'Авокадо'
@@ -240,19 +242,127 @@ class Test_unit_tests(unittest.TestCase):
 
     #def test_M17(self):
 
+    '''
+    def test_M18(self):
+        window = tk.Tk()
+        notebook = ttk.Notebook(window, width=800, height=800)
+        dish_nutrients_form = Dish_nutrients_form(notebook)
 
-    #def test_M18(self):
+        expected_result_0 = 'Огурец'
+        expected_result_1 = 300.0
+        expected_result_2 = 45.0
+        expected_result_3 = 1.95
+        expected_result_4 = 0.33
+        expected_result_5 = 10.89
+        expected_result_6 = 1
 
+        dish_nutrients_form.found_product_name_label.configure(text='Огурец')
+        dish_nutrients_form.selected_product_name = 'Огурец'
+        dish_nutrients_form.product_weight_stepper_input.insert(0, 300.0)
+        dish_nutrients_form.selected_product_nutrients_data['calories'] = 15.0
+        dish_nutrients_form.selected_product_nutrients_data['proteins'] = 0.65
+        dish_nutrients_form.selected_product_nutrients_data['fats'] = 0.11
+        dish_nutrients_form.selected_product_nutrients_data['carbohydrates'] = 3.63
+        dish_nutrients_form.add_product()
 
-    #def test_M19(self):
+        rows = dish_nutrients_form.dish_ingredients_table.get_children()
+        result_0 = dish_nutrients_form.dish_ingredients_table.item(rows[0])['values'][0]
+        result_1 = float(dish_nutrients_form.dish_ingredients_table.item(rows[0])['values'][5])
+        result_2 = float(dish_nutrients_form.dish_ingredients_table.item(rows[0])['values'][1])
+        result_3 = float(dish_nutrients_form.dish_ingredients_table.item(rows[0])['values'][2])
+        result_4 = float(dish_nutrients_form.dish_ingredients_table.item(rows[0])['values'][3])
+        result_5 = float(dish_nutrients_form.dish_ingredients_table.item(rows[0])['values'][4])
+        result_6 = len(rows)
 
+        self.assertEqual(expected_result_0, result_0)
+        self.assertEqual(expected_result_1, result_1)
+        self.assertEqual(expected_result_2, result_2)
+        self.assertEqual(expected_result_3, result_3)
+        self.assertEqual(expected_result_4, result_4)
+        self.assertEqual(expected_result_5, result_5)
+        self.assertEqual(expected_result_6, result_6)
+    '''
+    '''
+    def test_M19(self):
+        window = tk.Tk()
+        notebook = ttk.Notebook(window, width=800, height=800)
+        dish_nutrients_form = Dish_nutrients_form(notebook)
 
-    #def test_M20(self):
+        expected_result = 0
 
+        dish_nutrients_form.found_product_name_label.configure(text='')
+        dish_nutrients_form.selected_product_name = ''
+        dish_nutrients_form.product_weight_stepper_input.insert(0, '')
+        dish_nutrients_form.selected_product_nutrients_data = HTTPError
+        dish_nutrients_form.add_product()
 
-    #def test_M21(self):
+        rows = dish_nutrients_form.dish_ingredients_table.get_children()
+        result = len(rows)
 
+        self.assertEqual(expected_result, result)
+    '''
+    '''
+    def test_M20(self):
+        window = tk.Tk()
+        notebook = ttk.Notebook(window, width=800, height=800)
+        dish_nutrients_form = Dish_nutrients_form(notebook)
 
+        expected_result = 0
+
+        dish_nutrients_form.found_product_name_label.configure(text='Огурец')
+        dish_nutrients_form.selected_product_name = 'Огурец'
+        dish_nutrients_form.product_weight_stepper_input.insert(0, 300.0)
+        dish_nutrients_form.selected_product_nutrients_data['calories'] = 15.0
+        dish_nutrients_form.selected_product_nutrients_data['proteins'] = 0.65
+        dish_nutrients_form.selected_product_nutrients_data['fats'] = 0.11
+        dish_nutrients_form.selected_product_nutrients_data['carbohydrates'] = 3.63
+        dish_nutrients_form.add_product()
+        dish_nutrients_form.found_product_name_label.configure(text='Авокадо')
+        dish_nutrients_form.selected_product_name = 'Авокадо'
+        dish_nutrients_form.product_weight_stepper_input.insert(0, 200.0)
+        dish_nutrients_form.selected_product_nutrients_data['calories'] = 160.0
+        dish_nutrients_form.selected_product_nutrients_data['proteins'] = 2.0
+        dish_nutrients_form.selected_product_nutrients_data['fats'] = 14.66
+        dish_nutrients_form.selected_product_nutrients_data['carbohydrates'] = 8.53
+        dish_nutrients_form.add_product()
+
+        dish_nutrients_form.clear_table()
+
+        rows = dish_nutrients_form.dish_ingredients_table.get_children()
+        result = len(rows)
+
+        self.assertEqual(expected_result, result)
+    '''
+    '''
+    def test_M21(self):
+        window = tk.Tk()
+        notebook = ttk.Notebook(window, width=800, height=800)
+        daily_calories_standart = Daily_calories_standart(notebook)
+
+        expected_result = daily_calories_standart.activity_level_description_text.get('1.0', END)
+
+        daily_calories_standart.selected_activity_level.set(2)
+        daily_calories_standart.switch_activity_level_description()
+
+        result = daily_calories_standart.activity_level_description_text.get('1.0', END)
+
+        self.assertNotEqual(expected_result, result)
+    '''
+    '''
+    def test_M22(self):
+        window = tk.Tk()
+        notebook = ttk.Notebook(window, width=800, height=800)
+        daily_calories_standart = Daily_calories_standart(notebook)
+
+        expected_result = daily_calories_standart.activity_level_description_text.get('1.0', END)
+
+        daily_calories_standart.selected_activity_level.set(0)
+        daily_calories_standart.switch_activity_level_description()
+
+        result = daily_calories_standart.activity_level_description_text.get('1.0', END)
+
+        self.assertEqual(expected_result, result)
+    '''
     #def test_M22(self):
 
 
